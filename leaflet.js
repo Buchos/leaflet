67c66f6e11a5fd5ea30568e12e47282de3f71906
Buchos <pluyart@gmail.com>
2020-10-07 10:32:32 +0200

    map working

--- leaflet.js
+++ leaflet.js
@@ -1,6 +1,6 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([50.6412, 5.5718], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnVjaDBzIiwiYSI6ImNrZno0MDJvZDAwODQycXFkOWE1ZjlpajUifQ.fSbRbSfF7mpjw0feoSQQnw', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
@@ -9,7 +9,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
 
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
+var marker = L.marker([50.6412, 5.5718]).addTo(mymap);
 
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
